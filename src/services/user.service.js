d9dd418a9860985f6fd3b2e4799ea8b7ee44c113
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-30 20:43:34 +0530

    Feat: Added auth Middlewares

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -52,7 +52,34 @@ async function signin(data) {
   }
 }
 
+async function isAuthenticated(token) {
+  try {
+    if (!token) {
+      throw new AppError("Missing JWT token", StatusCodes.BAD_REQUEST);
+    }
+    const response = Auth.verifyToken(token);
+    const user = await userRepository.get(response.id);
+    if (!user) {
+      throw new AppError("No user found", StatusCodes.NOT_FOUND);
+    }
+    return user.id;
+  } catch (error) {
+    if (error instanceof AppError) throw error;
+    if (error.name == "JsonWebTokenError") {
+      throw new AppError("Invalid token", StatusCodes.BAD_REQUEST);
+    }
+    if (error.name == "TokenExpiredError") {
+      throw new AppError("token expired", StatusCodes.BAD_REQUEST);
+    }
+    throw new AppError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   create,
   signin,
+  isAuthenticated,
 };
