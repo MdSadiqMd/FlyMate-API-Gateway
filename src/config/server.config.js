a75b4f21d6940fb1bd1cdb11d90def702d94fa9c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-30 21:28:01 +0530

    Feat: Added Reverse Proxy

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -7,4 +7,6 @@ module.exports = {
   SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
   JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
   JWT_SECRET: process.env.JWT_SECRET || "Random",
+  FLIGHT_SERVICE: process.env.FLIGHT_SERVICE || "http://localhost:3000",
+  BOOKING_SERVICE: process.env.BOOKING_SERVICE || "http://localhost:4000",
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const config = require("./config/server.config.js");
 const logger = require("./config/logger.config.js");
@@ -6,8 +8,31 @@ const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 30,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
+
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: config.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" },
+  })
+);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: config.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingService": "/" },
+  })
+);
 
 app.use("/api", apiRoutes);
 
